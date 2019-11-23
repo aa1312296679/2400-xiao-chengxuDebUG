0bc57a4cc7b9d227ebd55854a7dfa9dbac3a4c83
unknown <1426683814@qq.com>
2019-11-23 18:27:46 +0800

    msg

--- pages/goodsinfo/index.js
+++ pages/goodsinfo/index.js
@@ -17,7 +17,13 @@ Page({
       { "url": "../../img/home/商品详情_02.jpg" },
       { "url": "../../img/home/商品详情_02.jpg" }
     ],
-    productData: null
+    productData: null,
+    userInfo: null,
+    type: null,
+    num: 1,
+    productId: null,
+    addressId: null,
+    remark: null
   },
 
   /**
@@ -25,7 +31,11 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
+    options.id = 1
     if (app.globalData.userInfo && app.globalData.userInfo.id) {
+      this.setData({
+        productId: options.id
+      })
       app.request({
         url: '/content/api/product-detail',
         data: {
@@ -33,9 +43,10 @@ Page({
           uid: app.globalData.userInfo.id
         }
       }).then(res => {
-        console.log(res)
         this.setData({
-          productData: res.data.product
+          productData: res.data.product,
+          userInfo: res.data,
+          addressId: res.data.userAddress.id
         })
       })
     }else {
@@ -52,7 +63,31 @@ Page({
       })
     }
   },
-
+  addCount() {
+    let temp = Number(this.data.num)
+    temp = ++temp
+    this.setData({
+      num: temp
+    })
+  },
+  delCount() {
+    let temp = Number(this.data.num)
+    if (temp > 1) temp = --temp
+    this.setData({
+      num: temp
+    })
+  },
+  goCart() {
+    this.setData({
+      type: 'cart',
+      isShow: true
+    })
+  },
+  chooseAddress() {
+    wx.navigateTo({
+      url: '/pages/My/address/index?type=shop',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -61,10 +96,51 @@ Page({
   },
   goBuy() {
     this.setData({
-      isShow: true
+      isShow: true,
+      type: null
+    })
+  },
+  closePlate() {
+    this.setData({
+      isShow: false
     })
   },
   closeBuy() {
+    let that = this
+    if (this.data.type) {
+      app.request({
+        url: '/content/api/cart-add',
+        data: {
+          uid: app.globalData.userInfo.id,
+          productId: this.data.productId,
+          number: this.data.num
+        }
+      }).then(res => {
+        wx.showToast({
+          title: '加入成功',
+          type:'success'
+        })
+      })
+    } else {
+      app.request({
+        url: '/content/api/create-order',
+        data: {
+          uid: app.globalData.userInfo.id,
+          productId: that.data.productId,
+          number: that.data.num,
+          integral: 0,
+          couponId: 0,
+          type: 2,
+          remark: that.data.remark,
+          addressId: that.data.addressId
+        }
+      }).then(res => {
+        wx.showToast({
+          title: '下单成功',
+          type:'success'
+        })
+      })
+    }
     this.setData({
       isShow: false
     })
