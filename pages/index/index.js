2fdf5c2395f12e2647c3da9823c48bda5cf44093
aa1312296679 <1312296679@qq.com>
2019-12-15 19:03:07 +0800

    已完成声音bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,8 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+//全屏缓冲
+var screenTimer=false;
 Page({
   data: {
     motto: 'Hello World',
@@ -10,11 +11,10 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     content:'',
     goodProduct:[],
-    bnrUrl: [
-      // { "url": "../../img/home/木马首页_04.png" }, 
-      // { "url": "../../img/home/木马首页_04.png" }, 
-      // { "url": "../../img/home/木马首页_04.png" }, 
-      // { "url": "../../img/home/木马首页_04.png" }
+    searchName:"",//搜索名称
+    bnrUrl: [  //0图片 1视频 获取数据信息后对数据结构重构为该形式
+      { id: '01', url:"http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",type:1,muted:true,screen:false},
+      { id: '02', url: "/mock/img/木马首页_04.png", type:0 }
     ]
   },
   //事件处理函数
@@ -45,13 +45,15 @@ Page({
       data:{
         page: 1
       },
-      success(res){
-        that.setData({
-          bnrUrl:res.data.data.advert,
-          content: res.data.data.logo.content,
-          goodProduct: res.data.data.goodProduct
-        })
-        console.log(res)
+      success:(res)=>{
+        console.log(res.data)
+        // console.log
+        // this.setData({
+        //   bnrUrl:res.data.data.advert,
+        //   content: res.data.data.logo.content,
+        //   goodProduct: res.data.data.goodProduct
+        // })
+        // console.log(res)
       }
     })
     if (app.globalData.userInfo) {
@@ -81,22 +83,26 @@ Page({
       })
     }
   },
+  /**
+   * 搜索处理
+   **/
   search(e){
+    console.log("search按钮");
     let search = e.detail.value
-    let that = this
+   
     wx.request({
       url: `${app.globalData.host}/content/api/index-search`,
       method:'post',
       data:{
         search: search
       },
-      success(res){
-        console.log(res)
-        if (res.data.code === 1) {
-          that.setData({
-            goodProduct: res.data.data
-          })
-        }
+      success:(res)=>{
+        console.log(res);
+        // if (res.data.code === 1) {
+        //   this.setData({
+        //     goodProduct: res.data.data
+        //   })
+        // }
       }
     })
   },
@@ -107,5 +113,44 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  videoHandle(e){
+    //获取被点击的视频控件的控件id
+    let videoId=e.currentTarget.dataset.vedioid;   
+    //获取控件索引
+    let controlIndex = e.currentTarget.dataset.index;
+    // console.log(controlIndex); 
+    this.screenPlay(videoId, controlIndex);                          
+  },
+  /**
+   * 全屏播放
+   * @param id 视频控件的控件id
+   * @param index 视频控件的控件索引
+   * **/ 
+  screenPlay(id,index){
+    //限制执行频率
+    if (screenTimer){
+      console.log("----");
+      return false;
+    }
+
+    screenTimer=setTimeout(()=>{
+      //启用视频声音或关闭视频声音
+      this.data.bnrUrl[index].muted = !this.data.bnrUrl[index].muted;
+      //全屏或关闭全屏
+      this.data.bnrUrl[index].screen = !this.data.bnrUrl[index].screen;
+      //
+      console.log(this.data.bnrUrl);
+      this.setData({
+        bnrUrl: this.data.bnrUrl
+      })
+      clearTimeout(screenTimer);
+      screenTimer=null;
+    },500);
+    
+    //执行全屏方法
+    // var videoContent=wx.createVideoContext(id,this);
+    // console.log(videoContent);
+    // videoContent.requestFullScreen();
   }
 })
